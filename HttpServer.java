5c4c4b4a5df2739e0272a8b31a1f6b104ddc5b78
Ezgi Karal <emkaral@gmail.com>
2012-11-21 12:17:26 +0000

    Added methods

--- HttpServer.java
+++ HttpServer.java
@@ -2,17 +2,80 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class HttpServer{
+public class HttpServer extends Thread{
+	final String HTMLStart=
+	"<html>"+"<title>Http Server</title>"+"<body>";
+	final String HTMLEnd=
+	"</body>"+"</html>";
+	
+	PrintWriter output=null; 
+	BufferedReader input=null;
+	Socket client=null;
+	
+	public HttpServer(Socket cclient){
+		client=cclient;
+	}
+	
+	public void run(){
+		String inputLine;
+		try{
+			System.out.println("The client"+client.getInetAddress()+":"+client.getPort()+"is connected");
+			output=new PrintWriter(client.getOutputStream(),true);
+			input=new BufferedReader(new InputStreamReader(client.getInputStream()));
+			output.flush();
+			while(!(inputLine=input.readLine()).isEmpty()){
+				StringTokenizer tokens=new StringTokenizer(inputLine);
+				if(tokens.hasMoreTokens()){
+					if(tokens.nextToken().equals("GET")){
+						String filename=tokens.nextToken().replace("/","");
+						if(filename.equals("")){
+							filename="index.html";
+						}
+						File newfile= new File("/"+filename);
+						System.out.println("Requested file was "+filename);
+						write(newfile,output);
+					
+					}
+				}
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
 		
+	}
+	public void write(File newfile, PrintWriter output){
+		String line;
+		if(newfile.exists()){
+			try{
+				FileReader freader=new FileReader(newfile);
+				BufferedReader breader= new BufferedReader(freader);
+				while((line=breader.readLine())!=null){
+					output.write(line);
+				}
+				output.flush();
+				breader.close();
+				System.out.println("File is written.");
+			}
+			catch (IOException e)
+				{
+					System.err.println("File could not be written.");
+					e.printStackTrace();
+					output.flush();
+								
+				}
+		}
+		else {
+			output.write("File could not be found.");
+			output.flush();
+			System.out.println("File not found.");
+		}
+	}
+		
+			
 	public static void main(String[] args) throws IOException{
 		ServerSocket serverSocket=null;
 		final int PortNo=80;
-		PrintWriter output=null; 
-		BufferedReader input=null;
-		Socket client=null; 
-		String inputLine;
-		String line;
-		
 		try{
 			serverSocket=new ServerSocket(PortNo);
 			
@@ -25,60 +88,21 @@ public class HttpServer{
 		while(true)
 		{
 			try{
-				client=serverSocket.accept();
+				Socket client=serverSocket.accept();
+				new HttpServer(client).start();
 			}
 			catch(IOException e){
 				System.err.println("Failed accept");
 				System.exit(1);
-			}
-			try{
-				System.out.println("The client"+client.getInetAddress()+":"+client.getPort()+"is connected");
-				output=new PrintWriter(client.getOutputStream(),true);
-				input=new BufferedReader(new InputStreamReader(client.getInputStream()));
-				while((inputLine=input.readLine())!=null){
-					StringTokenizer tokens=new StringTokenizer(inputLine);
-					if(tokens.hasMoreTokens()){
-						if(tokens.nextToken().equals("GET")){
-							String filename=tokens.nextToken().replace("/","");
-							if(filename.equals("")){
-								filename="index.html";
-							}
-							File newfile= new File("/"+filename);
-							System.out.println("Requested file was "+filename);
-							if(newfile!=null){
-								try{
-									FileReader freader=new FileReader(newfile);
-									BufferedReader breader= new BufferedReader(freader);
-									while((line=breader.readLine())!=null){
-										output.write(line);
-									}
-									breader.close();
-									System.out.println("File is written.");
-								}
-								catch (IOException e)
-								{
-								System.err.println("File could not be written.");
-								}
-							}
-							else {
-							System.out.println("File could not be found.");
-							}
-						}
-					}
-				}
-				output.close();
-				input.close();
-				client.close();
 				
 			}
-			catch(IOException e){
-			System.err.println("IO Exception occurred.");
-			}
-			
 		}
 	}
-
-}		
+}
+			
+							
+							
+	
 			
 			
 			
